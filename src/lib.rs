af8d7b90d3934db2a26dcd2443c8121dd2f8e820
Jon Magnuson <jon.magnuson@gmail.com>
2019-04-30 00:12:44 -0500

    deps: add tokio, dbus, etc.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+//!  ConnMan D-Bus API
+
+extern crate dbus;
+extern crate dbus_tokio;
+extern crate futures;
+extern crate qutex;
+extern crate tokio;
