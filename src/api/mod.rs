fc5b35ef3a0339172d6e58520392e47e527c216d
Jon Magnuson <jon.magnuson@gmail.com>
2019-05-12 20:25:28 -0500

    api: add `PropertyError`
    Currently handles missing properties as well as type casting errors.

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -9,10 +9,28 @@ pub mod technology;
 
 use dbus;
 
+use std::borrow::Cow;
+
 #[derive(Debug, Fail)]
 pub enum Error {
     #[fail(display = "{}", _0)]
     DbusError(#[cause] dbus::Error),
+    #[fail(display = "'{}'", _0)]
+    PropertyError(#[cause] PropertyError),
+}
+
+#[derive(Debug, Fail)]
+pub enum PropertyError {
+    #[fail(display = "Property not present: '{}'", _0)]
+    NotPresent(Cow<'static, str>),
+    #[fail(display = "Failed to cast property: '{}'", _0)]
+    Cast(Cow<'static, str>)
+}
+
+impl From<PropertyError> for Error {
+    fn from(e: PropertyError) -> Self {
+        Error::PropertyError(e)
+    }
 }
 
 impl From<dbus::Error> for Error {
