0faeea177062105f363d545a2414ece81b390f9f
Jon Magnuson <jon.magnuson@gmail.com>
2019-05-12 21:56:02 -0500

    api: add property convenience functions

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -8,8 +8,11 @@ pub mod service;
 pub mod technology;
 
 use dbus;
+use dbus::arg::{cast, RefArg, Variant};
 
 use std::borrow::Cow;
+use std::collections::HashMap;
+use std::str::FromStr;
 
 #[derive(Debug, Fail)]
 pub enum Error {
@@ -37,4 +40,30 @@ impl From<dbus::Error> for Error {
     fn from(e: dbus::Error) -> Self {
         Error::DbusError(e)
     }
+}
+
+/// Convenience function for getting property values.
+fn get_property<T: Clone + 'static>(
+    properties: HashMap<String, Variant<Box<RefArg + 'static>>>,
+    prop_name: &'static str,
+) -> Result<T, Error> {
+    properties.get(prop_name)
+        .ok_or(PropertyError::NotPresent(Cow::Borrowed(prop_name)).into())
+        .and_then(|variant|
+            cast::<T>(&variant.0).cloned()
+                .ok_or(PropertyError::Cast(Cow::Borrowed(prop_name)).into())
+        )
+}
+
+/// Convenience function for getting property values that impl `FromStr`.
+fn get_property_fromstr<T: FromStr + 'static>(
+    properties: HashMap<String, Variant<Box<RefArg + 'static>>>,
+    prop_name: &'static str,
+) -> Result<T, Error> {
+    properties.get(prop_name)
+        .ok_or(PropertyError::NotPresent(Cow::Borrowed(prop_name)).into())
+        .and_then(|variant| variant.as_str()
+            .and_then(|s| T::from_str(s).ok())
+            .ok_or(PropertyError::Cast(Cow::Borrowed(prop_name)).into())
+        )
 }
