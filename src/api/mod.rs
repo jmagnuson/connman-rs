2b56ea1f399265a3af7f20c51317c4b63ab61318
Jon Magnuson <jon.magnuson@gmail.com>
2019-08-19 12:13:51 -0500

    api: introduce FromProperties trait
    This will be used by Service and Technology to parse out their
    respective property fields into typed structures.
    
    Also adds `get_property_argiter`, to use for parsing out nested dict
    types from D-Bus.

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -15,6 +15,8 @@ use std::collections::HashMap;
 use std::str::FromStr;
 
 type RefArgMap = HashMap<String, Variant<Box<RefArg + 'static>>>;
+type RefArgMapRef<'a> = HashMap<String, &'a RefArg>;
+type RefArgIter<'a> = Box<Iterator<Item=&'a RefArg> + 'a>;
 
 #[derive(Debug, Fail)]
 pub enum Error {
@@ -68,4 +70,55 @@ fn get_property_fromstr<T: FromStr + 'static>(
             .and_then(|s| T::from_str(s).ok())
             .ok_or_else(|| PropertyError::Cast(Cow::Borrowed(prop_name)))
         )
+}
+
+/// Convenience function for getting property values from a Dict or Array.
+fn get_property_argiter<'a>(
+    properties: &'a RefArgMap,
+    prop_name: &'static str,
+) -> Result<RefArgIter<'a>, PropertyError> {
+    properties.get(prop_name)
+        .ok_or_else(|| PropertyError::NotPresent(Cow::Borrowed(prop_name)))
+        .and_then(|variant|
+            variant.0.as_iter()
+                .ok_or_else(|| PropertyError::Cast(Cow::Borrowed(prop_name)))
+        )
+}
+
+pub trait FromProperties: Sized {
+    fn from_properties(properties: &RefArgMap, prop_name: &'static str) -> Result<Self, PropertyError>;
+}
+
+impl FromProperties for String {
+    fn from_properties(properties: &RefArgMap, prop_name: &'static str) -> Result<Self, PropertyError> {
+        get_property_fromstr::<Self>(properties, prop_name)
+    }
+}
+
+impl <T: FromProperties + Clone + 'static> FromProperties for Vec<T> {
+    fn from_properties(properties: &RefArgMap, prop_name: &'static str) -> Result<Self, PropertyError> {
+        get_property::<Vec<T>>(properties, prop_name)
+    }
+}
+
+impl FromProperties for u8 {
+    fn from_properties(properties: &RefArgMap, prop_name: &'static str) -> Result<Self, PropertyError> {
+        get_property::<Self>(properties, prop_name)
+    }
+}
+
+impl FromProperties for bool {
+    fn from_properties(properties: &RefArgMap, prop_name: &'static str) -> Result<Self, PropertyError> {
+        get_property::<Self>(properties, prop_name)
+    }
+}
+
+impl <T: FromProperties> FromProperties for Option<T> {
+    fn from_properties(properties: &RefArgMap, prop_name: &'static str) -> Result<Self, PropertyError> {
+        match T::from_properties(properties, prop_name) {
+            Err(PropertyError::NotPresent(_)) => Ok(None),
+            Ok(s) => Ok(Some(s)),
+            res => res.map(Option::Some),
+        }
+    }
 }
