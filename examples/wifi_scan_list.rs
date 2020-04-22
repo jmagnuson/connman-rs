f6e38d152b3f577fe7c0be9caaf4cfebff23c7be
Jon Magnuson <jon.magnuson@gmail.com>
2020-04-21 22:31:52 -0500

    only try wifi_scan_list for now

--- examples/wifi_scan_list.rs
+++ examples/wifi_scan_list.rs
@@ -1,59 +1,54 @@
-extern crate connman;
-extern crate dbus;
-extern crate dbus_tokio;
-extern crate futures;
+use dbus_tokio::connection;
+use dbus::nonblock;
+use dbus::message::MatchRule;
+use futures::StreamExt;
+
+use connman::api::Error as ConnmanError;
+use connman::{Manager, Technology};
+
+use futures::future::{TryFuture, TryFutureExt};
+use tokio::time::timeout;
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    // Connect to the D-Bus session bus (this is blocking, unfortunately).
+    let (resource, conn) = connection::new_session_sync().unwrap();
+
+    // The resource is a task that should be spawned onto a tokio compatible
+    // reactor ASAP. If the resource ever finishes, you lost connection to D-Bus.
+    tokio::spawn(async {
+        let err = resource.await;
+        panic!("Lost connection to D-Bus: {}", err);
+    });
+
+    let manager = Manager::new(conn);
+
+    let wifi = get_technology_wifi(&manager).await.unwrap().unwrap();
+
+    let services = timeout(
+        Duration::from_secs(5),
+        wifi.scan()
+    ).await.unwrap();
+
+    for svc in services {
+        // Dump service info
+        println!("Found service: {:?}", svc)
+    }
+
+    Ok(())
+}
 
 use std::borrow::Cow;
 use std::rc::Rc;
 use std::time::Duration;
 
-use connman::api::Error as ConnmanError;
-use connman::{Manager, Technology};
-use dbus::{BusType, Connection};
-use dbus_tokio::AConnection;
-use futures::Future;
-use tokio::prelude::FutureExt;
-use tokio::reactor::Handle;
-use tokio::runtime::current_thread::Runtime;
-
-pub fn get_technology_wifi(
+pub async fn get_technology_wifi(
     manager: &Manager,
-) -> impl Future<Item = Option<Technology>, Error = ConnmanError> {
-    manager
-        .get_technologies()
-        // Filter out the wifi technology (eventually this will be a simple library call)
-        .map(|v| {
-            v.into_iter().find(move |t| {
-                t.props.type_ == connman::api::technology::Type::Wifi
-            })
-        })
-}
+) -> Result<Option<Technology>, ConnmanError> {
+    let technologies = manager.get_technologies().await?;
 
-fn main() {
-    let mut runtime = Runtime::new().unwrap();
-
-    let conn = Rc::new(Connection::get_private(BusType::System).unwrap());
-    let aconn = Rc::new(AConnection::new(conn.clone(), Handle::default(), &mut runtime).unwrap());
-
-    let manager = Manager::new(aconn);
-
-    let wifi_scan = get_technology_wifi(&manager)
-        // Initiate scan
-        .and_then(|wifi| wifi.unwrap().scan()
-            .timeout(Duration::from_secs(10))
-                .map_err(|e| {
-                    let s = format!("{:?}", e);
-                    ConnmanError::Timeout(Cow::Owned(s))
-                }))
-    // List services once scan completes
-    .and_then(move |_| manager.clone().get_services())
-        .and_then(|services| {
-            for svc in services {
-                // Dump service info
-                println!("Found service: {:?}", svc)
-            }
-            Ok(())
-        });
-
-    runtime.block_on(wifi_scan).unwrap();
+    Ok(technologies.into_iter().find(move |t| {
+        t.props.type_ == connman::api::technology::Type::Wifi
+    }))
 }
