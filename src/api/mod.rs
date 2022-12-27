8b2373581cfa283a0e2d6e9d029ae30d929c6461
Vladislav Zhukov <vlad.zhukov@elektron.se>
2022-12-27 17:44:54 +0100

    ref: Fix clippy warnings

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -48,11 +48,11 @@ fn get_property<T: Clone + 'static>(
 ) -> Result<T, PropertyError> {
     properties
         .get(prop_name)
-        .ok_or_else(|| PropertyError::NotPresent(Cow::Borrowed(prop_name)))
+        .ok_or(PropertyError::NotPresent(Cow::Borrowed(prop_name)))
         .and_then(|variant| {
             cast::<T>(&variant.0)
                 .cloned()
-                .ok_or_else(|| PropertyError::Cast(Cow::Borrowed(prop_name)))
+                .ok_or(PropertyError::Cast(Cow::Borrowed(prop_name)))
         })
 }
 
@@ -63,12 +63,12 @@ fn get_property_fromstr<T: FromStr + 'static>(
 ) -> Result<T, PropertyError> {
     properties
         .get(prop_name)
-        .ok_or_else(|| PropertyError::NotPresent(Cow::Borrowed(prop_name)))
+        .ok_or(PropertyError::NotPresent(Cow::Borrowed(prop_name)))
         .and_then(|variant| {
             variant
                 .as_str()
                 .and_then(|s| T::from_str(s).ok())
-                .ok_or_else(|| PropertyError::Cast(Cow::Borrowed(prop_name)))
+                .ok_or(PropertyError::Cast(Cow::Borrowed(prop_name)))
         })
 }
 
@@ -79,12 +79,12 @@ fn get_property_argiter<'a>(
 ) -> Result<RefArgIter<'a>, PropertyError> {
     properties
         .get(prop_name)
-        .ok_or_else(|| PropertyError::NotPresent(Cow::Borrowed(prop_name)))
+        .ok_or(PropertyError::NotPresent(Cow::Borrowed(prop_name)))
         .and_then(|variant| {
             variant
                 .0
                 .as_iter()
-                .ok_or_else(|| PropertyError::Cast(Cow::Borrowed(prop_name)))
+                .ok_or(PropertyError::Cast(Cow::Borrowed(prop_name)))
         })
 }
 
